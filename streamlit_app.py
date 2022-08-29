edfa401e5ab6f17c1fe45926ae549f3df3798788
caioagleal <112430742+caioagleal@users.noreply.github.com>
2022-08-29 17:30:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 """
 # Welcome to Streamlit!
@@ -16,23 +19,26 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+t = np.linspace(0, 0.05, 100)
+v = np.sqrt(2) * 220 * np.sin(2 * np.pi * 60 * t)
+media = np.mean(v)
+media
+fig = plt.figure(dpi=100)
+plt.plot(t, v)
+plt.grid()
+plt.xlabel('$t$')
+plt.ylabel('v')
+plt.title('Função 1')
+plt.show()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#w = (np.sqrt(2) * 220 * np.sin(2 * np.pi * 60 * t))**2
+w = v**2
+media2 = np.mean(w)
+media2
+fig = plt.figure(dpi=100)
+plt.plot(t, w)
+plt.grid()
+plt.xlabel('$t$')
+plt.ylabel('w')
+plt.title('Função 2')
+plt.show()
